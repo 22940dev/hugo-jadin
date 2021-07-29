58ebdf4c3201ef6d24f7a199dcab2978b6cc37d7
Taylor Jadin <jadin.taylor@gmail.com>
2021-07-28 20:34:46 -0500

    borked deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 host=$(head -1 deploy.config)
 ssh=$(tail -1 deploy.config)
 
-if [ "$HOSTNAME" = $host
+if [ "$HOSTNAME"  = $host ]
     cd /home/jadinme/jadin.me && git pull && hugo -d ~/public_html
 else
     ssh $ssh bash /home/jadinme/jadin.me/deploy.sh
